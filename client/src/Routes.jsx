72ee98fa1390439cbc70bb3713e9838a1938f625
Vusal <vusalcs54@gmail.com>
2024-08-28 23:13:43 +0400

    Added Chat component to Routes.jsx and created Chat.jsx

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -2,12 +2,13 @@ import Register from "./RegisterAndLoginForm.jsx";
 import { useContext } from "react";
 import { UserContext } from "./UserContext.jsx";
 import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
+import Chat from "./Chat.jsx";
 
 export default function Routes() {
   const { username, id } = useContext(UserContext);
 
   if (username) {
-    return 'logged in!' + username;
+    return <Chat />
   }
 
   return (
