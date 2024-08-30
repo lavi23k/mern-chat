b23d9b538575879b8cfe8fe99218c75e7aaf6dbc
Vusal <vusalcs54@gmail.com>
2024-08-30 16:59:30 +0400

    Implement user chat functionality

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,17 +1,21 @@
 import { useContext, useEffect, useState } from "react";
-import Avatar  from "./Avatar";
-import Logo from "./Logo"
+import Avatar from "./Avatar";
+import Logo from "./Logo";
 import { UserContext } from "./UserContext";
+import uniqBy from 'lodash/uniqBy';
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
   const [selectedUserId, setSelectedUserId] = useState(null);
-  const {username, id} = useContext(UserContext);
+  const [newMessageText, setNewMessageText] = useState('');
+  const [messages, setMessages] = useState([]);
+  const { username, id } = useContext(UserContext);
+
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:4000');
     setWs(ws);
-    ws.addEventListener('message', handleMessage)
+    ws.addEventListener('message', handleMessage);
   }, []);
 
   function showOnlinePeople(peopleArray) {
@@ -26,24 +30,48 @@ export default function Chat() {
     const messageData = JSON.parse(ev.data);
     if ('online' in messageData) {
       showOnlinePeople(messageData.online);
+    } else if ('text' in messageData) {
+      setMessages(prev => ([...prev, { ...messageData }]));
     }
+  }
+
+  function sendMessage(ev) {
+    ev.preventDefault();
+    ws.send(JSON.stringify({
+      recipient: selectedUserId,
+      text: newMessageText,
+    }));
+    setNewMessageText('');
+    setMessages(prev => ([
+      ...prev,
+      {
+        text: newMessageText,
+        sender: id,
+        recipient: selectedUserId,
+      }
+    ]));
   }
 
   const onLinePeopleExclOurUser = { ...onlinePeople };
   delete onLinePeopleExclOurUser[id];
+
+  const messagesWithoutDupes = uniqBy(messages, 'id');
 
   return (
     <div className="flex h-screen">
       <div className="bg-white w-1/3">
         <Logo />
         {Object.keys(onLinePeopleExclOurUser).map(userId => (
-          <div key={userId} onClick={() => setSelectedUserId(userId)} 
-              className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer "+(userId === selectedUserId ? 'bg-blue-100' : '')}>
+          <div
+            key={userId}
+            onClick={() => setSelectedUserId(userId)}
+            className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer " + (userId === selectedUserId ? 'bg-blue-100' : '')}
+          >
             {userId === selectedUserId && (
               <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
             )}
             <div className="flex gap-2 py-2 pl-3 items-center">
-              <Avatar username={onlinePeople[userId]} userId={userId}/>
+              <Avatar username={onlinePeople[userId]} userId={userId} />
               <span className="text-gray-800">{onlinePeople[userId]}</span>
             </div>
           </div>
@@ -56,15 +84,36 @@ export default function Chat() {
               <div className="text-gray-400">&larr; Select a person from the sidebar</div>
             </div>
           )}
+          {!!selectedUserId && (
+            <div className="overflow-y-scroll">
+              {messagesWithoutDupes.map(message => (
+                <div className={(message.sender === id ? 'text-right' : 'text-left')}>
+                  <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " + (message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
+                    sender: {message.sender}<br />
+                    my id: {id} <br />
+                    {message.text}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-        <div className="flex gap-2">
-          <input type="text" placeholder="Type your message here" className="bg-white flex-grow border rounded-sm p-2" />
-          <button className="bg-blue-500 p-2 text-white rounded-sm">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-            </svg>
-          </button>
-        </div>
+        {!!selectedUserId && (
+          <form className="flex gap-2" onSubmit={sendMessage}>
+            <input
+              type="text"
+              value={newMessageText}
+              onChange={ev => setNewMessageText(ev.target.value)}
+              placeholder="Type your message here"
+              className="bg-white flex-grow border rounded-sm p-2"
+            />
+            <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
